b9eb459aa31f791dd31a7fc101f7af09e4d8a243
Sidharth Sethia <sethiasidharth@gmail.com>
2016-12-26 18:28:32 +0530

    Create xml for splash screen and login/signup screen.

---
+++ app/src/main/java/com/medcords/mhcpanel/AgentLoginActivity.java
@@ -0,0 +1,13 @@
+package com.medcords.mhcpanel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgentLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agent_login);
+    }
+}
