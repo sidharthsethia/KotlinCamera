36002725b5479b7d04edadbe12e4c72c935ad2df
Sidharth Sethia <sethiasidharth@gmail.com>
2017-02-27 00:29:38 +0530

    Show exact number of patients found.

--- app/src/main/java/com/medcords/mhcpanel/activities/PatientListActivity.java
+++ app/src/main/java/com/medcords/mhcpanel/activities/PatientListActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.widget.TextView;
 
+import com.google.android.gms.vision.text.Text;
 import com.medcords.mhcpanel.R;
 import com.medcords.mhcpanel.adapters.PatientListRecyclerViewAdapter;
 import com.medcords.mhcpanel.models.Patient;
@@ -14,6 +17,7 @@ import java.util.ArrayList;
 public class PatientListActivity extends AppCompatActivity {
 
     private RecyclerView patientRecyclerView;
+    private TextView patientTitleTextView;
     private PatientListRecyclerViewAdapter patientListRecyclerViewAdapter;
     private ArrayList<Patient> patients;
 
@@ -23,10 +27,14 @@ public class PatientListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_patient_list);
 
         patientRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        patientTitleTextView = (TextView) findViewById(R.id.patient_list_title);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         patientRecyclerView.setLayoutManager(layoutManager);
 
         makePatientList();
+
+        String patient_list_title = String.format(getResources().getString(R.string.patient_list_title), Integer.toString(patients.size()));
+        patientTitleTextView.setText(Html.fromHtml(patient_list_title));
 
         patientListRecyclerViewAdapter = new PatientListRecyclerViewAdapter(this,patients);
 
